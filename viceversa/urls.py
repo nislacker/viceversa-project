e9c759b6a305935f9c8993a5ee088e2f9d9120e0
Nislacker <nislacker@gmail.com>
2020-11-17 21:25:35 +0200

    Create textarea and function to reverse inserted text

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reverse/', views.reverse, name='reverse'),
     path('', views.home),
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
     # return HttpResponse('Hello!')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_user_text = user_text[::-1]
+    words = user_text.split()
+    words_count = len(words)
+    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_user_text': reversed_user_text, 'words_count': words_count})
